d6179ff63e20b7683ef648a64ae82b18285b940b
mpaul-iecs <monish.iecs@gmail.com>
2026-09-07 17:22:02 +0530

    finalize1

--- src/views/AuthorView.jsx
+++ src/views/AuthorView.jsx
@@ -55,7 +55,6 @@ export default function AuthorView({ documentId, initialHtml, headerHtml, footer
         role={role}
         onReady={(editor) => { editorRef.current = editor; }}
         onDirty={() => { dirtyRef.current = true; }}
-        onNotify={onNotify}
         onHeaderFooterChange={(kind, html) => {
           hfRef.current = { ...hfRef.current, [kind]: html };
           dirtyRef.current = true;
